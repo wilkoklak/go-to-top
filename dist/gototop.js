9f5138d655480be1abc52672b30958186ce27fa1
Artur <thereaperv11@gmail.com>
2017-02-07 22:21:41 +0100

    Backend

--- dist/gototop.js
+++ dist/gototop.js
@@ -9,6 +9,7 @@ var _GTT_DEFAULTS = {
 	minScroll: false,
 	customToggledClass: false,
 	customElem: false,
+	scrollDuration: 1000,
 	instant: false
 };
 
@@ -24,17 +25,66 @@ var GoToTop = function () {
 	function GoToTop(config) {
 		_classCallCheck(this, GoToTop);
 
-		this.config = config;
+		this.config = config || _GTT_DEFAULTS;
 		this.init();
 	}
 
 	_createClass(GoToTop, [{
 		key: 'init',
 		value: function init() {
+			var _this = this;
+
 			if (this.config.customElem) {
 				this.buttonElem = this.config.customElem;
 			} else {
 				this.buttonElem = document.createElement('div');
+			}
+			this.buttonElem.addEventListener('click', function () {
+				_this.scrollToTop(_this);
+			});
+			this.buttonElem.classList.add('go-to-top');
+			document.body.appendChild(this.buttonElem);
+			window.addEventListener('scroll', function () {
+				_this.checkScroll(_this);
+			});
+		}
+	}, {
+		key: 'checkScroll',
+		value: function checkScroll() {
+			if (window.scrollY > (this.config.minScroll || window.innerHeight)) {
+				this.buttonElem.classList.add(this.config.customToggledClass || 'go-to-top-toggled');
+			} else {
+				this.buttonElem.classList.remove(this.config.customToggledClass || 'go-to-top-toggled');
+			}
+		}
+	}, {
+		key: 'scrollToTop',
+		value: function scrollToTop() {
+			var _this2 = this;
+
+			if (this.config.instant) {
+				window.scrollTo(0, 0);
+			} else {
+				(function () {
+					// http://stackoverflow.com/a/24559613/5899621
+					var cosParam = window.scrollY / 2;
+					var scrollCount = 0;
+					var oldTimestamp = performance.now();
+
+					var step = function step(newTimestamp) {
+						scrollCount += Math.PI / (_this2.config.scrollDuration / (newTimestamp - oldTimestamp));
+						if (scrollCount >= Math.PI) {
+							window.scrollTo(0, 0);
+						}
+						if (window.scrollY === 0) {
+							return;
+						}
+						window.scrollTo(0, Math.round(cosParam + cosParam * Math.cos(scrollCount)));
+						oldTimestamp = newTimestamp;
+						window.requestAnimationFrame(step);
+					};
+					window.requestAnimationFrame(step);
+				})();
 			}
 		}
 	}]);
--- src/gototop.js
+++ src/gototop.js
@@ -3,6 +3,7 @@ const _GTT_DEFAULTS = {
 	minScroll: false,
 	customToggledClass: false,
 	customElem: false,
+	scrollDuration: 1000,
 	instant: false
 }
 
@@ -17,7 +18,7 @@ function _GTT_IS_TYPE(elem, type) {
 
 class GoToTop {
 	constructor(config) {
-		this.config = config;
+		this.config = config || _GTT_DEFAULTS;
 		this.init();
 	}
 	init() {
@@ -25,6 +26,45 @@ class GoToTop {
 			this.buttonElem = this.config.customElem;
 		} else {
 			this.buttonElem = document.createElement('div');
+		}
+		this.buttonElem.addEventListener('click', () => {
+			this.scrollToTop(this)
+		})
+		this.buttonElem.classList.add('go-to-top');
+		document.body.appendChild(this.buttonElem);
+		window.addEventListener('scroll', () => {
+			this.checkScroll(this)
+		});
+	}
+	checkScroll() {
+		if(window.scrollY > (this.config.minScroll || window.innerHeight)) {
+			this.buttonElem.classList.add((this.config.customToggledClass || 'go-to-top-toggled'));
+		} else {
+			this.buttonElem.classList.remove((this.config.customToggledClass || 'go-to-top-toggled'))
+		}
+	}
+	scrollToTop() {
+		if(this.config.instant) {
+			window.scrollTo(0, 0);
+		} else {
+			// http://stackoverflow.com/a/24559613/5899621
+			let cosParam = window.scrollY / 2;
+			let scrollCount = 0;
+			let oldTimestamp = performance.now();
+
+			let step = (newTimestamp) => {
+				scrollCount += Math.PI / (this.config.scrollDuration / (newTimestamp - oldTimestamp));
+				if(scrollCount >= Math.PI) {
+					window.scrollTo(0, 0);
+				}
+				if(window.scrollY === 0) {
+					return;
+				}
+				window.scrollTo(0, Math.round(cosParam + cosParam * Math.cos(scrollCount)));
+				oldTimestamp = newTimestamp;
+				window.requestAnimationFrame(step);
+			}
+			window.requestAnimationFrame(step);
 		}
 	}
 }
